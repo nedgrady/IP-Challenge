6e2dddf5a5b3ebd8a3040d88f44e9bb196e843ae
ned grady <ned.grady@webappuk.com>
2016-09-09 13:28:10 +0100

    Added basic user input and image box. Added some logic behind user input

--- MyApplication/app/src/main/java/com/example/nedgrady/ipchallenge/Clue.java
+++ MyApplication/app/src/main/java/com/example/nedgrady/ipchallenge/Clue.java
@@ -1,13 +1,115 @@
 package com.example.nedgrady.ipchallenge;
 
+import android.net.Uri;
+import android.support.v4.view.ViewGroupCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
+import android.widget.*;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class Clue extends AppCompatActivity {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private int currentLevel=1;
+    private LevelData currentLevelData;
+
+    private TextView userTextView;
+    private EditText userEditText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clue);
+        super.onCreate(savedInstanceState);
+        userTextView = (TextView) findViewById(R.id.textView);
+        userEditText = (EditText) findViewById(R.id.editText);
+        nextLevel();
+        addActionListeners();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    private LevelData nextLevel() {
+        //Get the level data: the image, the answer and the hint
+        return new LevelData(currentLevel++);
+    }
+
+    private void onSubmit(TextView v){
+        if(getUserAnswer() == currentLevelData.getAnswer()){
+            currentLevelData = nextLevel();
+        }
+    }
+
+    /**
+     * @return String - text that user has input into the answer TextView
+     */
+    private String getUserAnswer(){
+        return userTextView.getText().toString();
+    }
+
+    private void addActionListeners(){
+        userEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                //if the tick button is clicked
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    onSubmit(v);
+                }
+                return false;
+            }
+        });
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Clue Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.nedgrady.ipchallenge/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Clue Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.nedgrady.ipchallenge/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
---
+++ MyApplication/app/src/main/java/com/example/nedgrady/ipchallenge/LevelData.java
@@ -0,0 +1,27 @@
+package com.example.nedgrady.ipchallenge;
+
+import android.media.Image;
+
+/**
+ * Created by ned.grady on 09/09/2016.
+ */
+public class LevelData {
+
+
+
+   public LevelData(int id){
+
+   }
+
+    public int getImage(){
+        return 0;
+    }
+
+    public String getClue(){
+        return "clue";
+    }
+
+    public String getAnswer(){
+        return "a";
+    }
+}
