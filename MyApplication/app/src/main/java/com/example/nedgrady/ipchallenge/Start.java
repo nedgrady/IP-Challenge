01e0ba87bc5ee4e3686b9bc02022b45028abd325
Jake Williams <jake.williams@webappuk.com>
2016-09-11 13:00:56 +0100

    AndroidManifest.xml, added xml for the start activity and clue activity. Start.java, added on start and on stop methods.

--- MyApplication/app/src/main/java/com/example/nedgrady/ipchallenge/Start.java
+++ MyApplication/app/src/main/java/com/example/nedgrady/ipchallenge/Start.java
@@ -3,12 +3,17 @@ package com.example.nedgrady.ipchallenge;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.Locale;
 
@@ -17,14 +22,14 @@ import java.util.Locale;
  */
 public class Start extends AppCompatActivity {
 
+    private GoogleApiClient client;
     private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clue);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
+        setContentView(R.layout.activity_start);
         TextView tx = (TextView)findViewById(R.id.title);
         Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/jamscript.ttf");
         tx.setTypeface(custom_font);
@@ -36,5 +41,43 @@ public class Start extends AppCompatActivity {
                 startActivity(new Intent(Start.this, Clue.class));
             }
         });
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Clue Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.nedgrady.ipchallenge/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Clue Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.nedgrady.ipchallenge/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
