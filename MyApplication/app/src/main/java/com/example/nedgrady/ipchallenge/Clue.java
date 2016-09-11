4866baa234260cea22ec91fcb567469f1f758dba
Lauren Weston <lauren.weston@webappuk.com>
2016-09-11 13:55:29 +0100

    Neaten up code

--- MyApplication/app/src/main/java/com/example/nedgrady/ipchallenge/Clue.java
+++ MyApplication/app/src/main/java/com/example/nedgrady/ipchallenge/Clue.java
@@ -38,7 +38,7 @@ public class Clue extends AppCompatActivity {
     private Button hintButtonText;
     private Button hintButtonZoom;
     private CountDownTimer s;
-    private long score;
+    private long score = 100;
     private long seconds;
 
     @Override
@@ -46,6 +46,7 @@ public class Clue extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clue);
 
+        //Page Elements
         levelText = (TextView) findViewById(R.id.level);
         userImageView = (ImageView) findViewById(R.id.imageView);
         hintText = (TextView) findViewById(R.id.hint);
@@ -54,15 +55,18 @@ public class Clue extends AppCompatActivity {
         inputField = (EditText) findViewById(R.id.inputText);
         hintButtonText = (Button) findViewById(R.id.hintButtonText);
         hintButtonZoom = (Button) findViewById(R.id.hintButtonZoom);
+
         //Set font
         Typeface font= Typeface.createFromAsset(getAssets(),"fonts/tradewinds.ttf");
         hintText.setTypeface(font);
         levelText.setTypeface(font);
         countDownTextView.setTypeface(font);
+        inputField.setTypeface(font);
+
+        // Get up the first level
         currentLevelData = nextLevel();
         showLevel(currentLevelData);
-        score = 100;
-        inputField.setTypeface(font);
+
 
         // The time remaining
         s = new CountDownTimer(50000, 1000) {
@@ -80,17 +84,20 @@ public class Clue extends AppCompatActivity {
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    //Get the image, the answer and the hint for the next level
     private LevelData nextLevel() {
-        Log.d("", "Next Level!");
         levelText.setText("Level " + ++currentLevel);
-        //Get the level data: the image, the answer and the hint
         return new LevelData(currentLevel, this);
     }
+
+
     private void onSubmit(TextView v){
         //Checking whether the user's answer is correct
         Log.d("action", "Checking if the users answer, '" + getUserAnswer() + "' is correct (" + currentLevelData.getAnswer()+ ")");
-        if(getUserAnswer().toLowerCase().equals(currentLevelData.getAnswer().toLowerCase())){
 
+        if(getUserAnswer().toLowerCase().equals(currentLevelData.getAnswer().toLowerCase())){
+            //Increase score
             scoreUp(seconds * 20);
             //If you've still got more levels to play, get the next level
             if(currentLevel < MAX_LEVELS){
@@ -110,29 +117,37 @@ public class Clue extends AppCompatActivity {
                 endGame();
             }
         }else{
+            //Decrease score if you entered a wrong answer
             scoreDown(100);
         }
         resetUI();
     }
+
     private void endGame() {
         hintText.setText("Well Done");
     }
+
     /*
      *@param currentLevelData LevelData - the level to show to the screen.
+     *
+     * Make the image appear
      */
     private void showLevel(LevelData currentLevelData) {
-        //Make image appear
         userImageView.setImageResource(currentLevelData.getImage());
     }
+
+    //Clear the text that the user has entered
     private void resetUI(){
         inputField.setText("");
     }
+
     /**
      * @return String - text that user has input into the answer TextView
      */
     private String getUserAnswer(){
         return inputField.getText().toString();
     }
+
     private void addActionListeners(){
         inputField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -174,41 +189,5 @@ public class Clue extends AppCompatActivity {
     private void scoreDown(long score){
         this.score -= score;
         scoreText.setText(Long.toString(this.score));
-    }
-    @Override
-    public void onStart() {
-        super.onStart();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Clue Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.nedgrady.ipchallenge/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-    @Override
-    public void onStop() {
-        super.onStop();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Clue Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.nedgrady.ipchallenge/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 }
